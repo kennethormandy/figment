4b8a5e64c0d69dc36767a7f75068d81a0e2afb42
Kenneth Ormandy <kenneth@chloi.io>
2015-07-20 18:29:18 -0700

    Adds failing diff test as an example

--- test/visual.js
+++ test/visual.js
@@ -1,51 +1,52 @@
-// Require and initialise PhantomCSS module
-// Paths are relative to CasperJs directory
-
-var fs = require('fs')
-var phantomcss = require('phantomcss')
 var Nightmare = require('nightmare')
 var should = require('should')
+var hux = require('huxley')
 var harp = require('harp')
+var fs = require('fs')
+var path = require('path')
+var resemble = require('node-resemble')
 
-describe('nightmare', function() {
+describe('nightmare', function () {
 
   // Still need to make this work from a clean install
   // KSS, etc.
   before(function(done) {
-    harp.server(__dirname + '/../examples', { port: 9005 }, done)
-  })
+    harp.server(__dirname + '/../examples', { port: 9005 })
 
-  it('Should load the styleguide', function (done) {
     new Nightmare()
-    .goto('http://localhost:9005')
-    .evaluate(function() {
-      return document.querySelector('h1').innerText
-    }, function(el) {
-      el.should.equal('KSS Style Guide')
-    })
-    .run(done)
+      .goto('http://google.com')
+      .screenshot(path.join('./test/fixtures', '1.png'))
+      .run(done)
   })
 
-  // it('Should take a screenshot diff', function (done) {
-  //   phantomcss.init({
-  //     // rebase: casper.cli.get('rebase'),
-  //     // casper: nightmare,
-  //     libraryRoot: fs.absolute(fs.workingDirectory + ''),
-  //     screenshotRoot: fs.absolute(fs.workingDirectory + '/test/fixtures/'),
-  //     failedComparisonsRoot: fs.absolute(fs.workingDirectory + '/test/fixtures/failed'),
-  //     addLabelToFailedImage: false,
-  //   })
-  //
+  // it('Should load the styleguide', function (done) {
   //   new Nightmare()
-  //   .goto('http://localhost:9005/section-figment')
-  //   .evaluate(function() {
-  //     phantomcss.screenshot('kssref-figment-children', 'figure caption with children')
-  //   }, function() {
-  //     phantomcss.compareAll()
-  //     // phantomcss.getExitStatus() // pass or fail?
-  //
-  //   })
-  //   .run(done)
+  //     .goto('http://localhost:9005')
+  //     .evaluate(function () {
+  //       return document.querySelector('h1').innerText
+  //     }, function(el) {
+  //       el.should.equal('KSS Style Guide')
+  //     })
+  //     .run(done)
   // })
 
+
+  it('Should take a screenshot diff', function (done) {
+
+    var fileTest = './test/fixtures/1.png'
+    var fileRef = './test/fixtures/reference/1.png'
+
+    resemble(fileTest).compareTo(fileRef).ignoreColors().onComplete(function(data) {
+      var misMatch = parseInt(data.misMatchPercentage, 10)
+
+      // should be the same dimensions
+      data.isSameDimensions.should.be.ok
+
+      // should match exactly
+      misMatch.should.equal(0)
+
+      done()
+    });
+
+  })
 })
